b5f9646d96ca66b21a628d6ee53195c7932b22a9
Karen Posada <k100posada@gmail.com>
2019-05-05 17:13:59 -0500

    feat(copy-overlay): animation

--- src/colorbox.js
+++ src/colorbox.js
@@ -66,19 +66,65 @@ const CopyOverlay = styled.div`
   z-index: 0;
   width: 100%;
   height: 100%;
-  &:hover {
+  transform: scale(0.1);
+  transition: transform 0.6s ease-in-out;
+  &.show {
     opacity: 1;
     transform: scale(50);
     z-index: 10;
+    position: absolute;
+  }
+`;
+
+const CopyMessage = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 4rem;
+  transform: scale(0.1);
+  opacity: 0;
+  color: white;
+  & > p {
+    font-weight: 400;
+    text-shadow: 1px 2px black;
+    background: rgba(255, 255, 255, 0.2);
+    width: 100%;
+    text-align: center;
+    margin-bottom: 0;
+    padding: 10px;
+  }
+  & > span {
+    font-size: 1.5rem;
+    color: #fafafa;
+  }
+  &.show-message {
+    opacity: 1;
+    transform: scale(1);
+    z-index: 20;
+    transition: all 0.4s ease-in-out;
+    transition-delay: 0.3s;
   }
 `;
 
 const ColorBox = ({ name, background }) => {
   const [copied, setCopied] = useState(false);
+  const changeCopyState = () => {
+    setCopied(true);
+    const callbackCopied = () => {
+      return setTimeout(() => setCopied(false), 1500);
+    };
+    callbackCopied();
+  };
+  console.log(copied);
   return (
-    <CopyToClipboard text={background}>
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
       <StyledDiv style={{ background: background }}>
-        <CopyOverlay style={{ background: background }} />
         <CopyContainer>
           <CopyButton>Copy</CopyButton>
         </CopyContainer>
@@ -88,6 +134,11 @@ const ColorBox = ({ name, background }) => {
           </BoxContent>
           <SeeMore>More</SeeMore>
         </AlignBottom>
+        <CopyOverlay style={{ background: background }} className={copied && 'show'} />
+        <CopyMessage className={copied && 'show-message'}>
+          <p>Copied!</p>
+          <span>{background}</span>
+        </CopyMessage>
       </StyledDiv>
     </CopyToClipboard>
   );
